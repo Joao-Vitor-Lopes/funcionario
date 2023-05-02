19c1f5aec97b3367cd9a7cc6420181a0f7ce7658
Joao-Vitor-Lopes <111802475+Joao-Vitor-Lopes@users.noreply.github.com>
2023-05-02 17:07:18 -0300

    Add files via upload

---
+++ Funcionario.php
@@ -0,0 +1,21 @@
+<?php
+class Funcionario{
+    public $nome;
+    public $cpf;
+    public $endereco;
+    public $telefone;
+    public $funcao;
+    public $salario;
+    public $turno;
+    public $email;
+
+    public function mostrarDados(){
+        
+    }
+
+    public function calculaDesconto($desconto){
+       
+    }
+} 
+
+?>
